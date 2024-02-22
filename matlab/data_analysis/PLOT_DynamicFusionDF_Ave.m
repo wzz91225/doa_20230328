a46884462f9a348912f8ed9def824fcdeb70c2ff
wzz91225 <wzz91225@outlook.com>
2024-02-22 19:00:47 +0800

    add figure angle error

--- matlab/data_analysis/PLOT_DynamicFusionDF_Ave.m
+++ matlab/data_analysis/PLOT_DynamicFusionDF_Ave.m
@@ -23,10 +23,10 @@ end
 load(matFilePath);
 % 检查是否含有需要的变量
 if not(exist('doa_phase_angle', 'var') && ...
-        exist('doa_amplitude_angle', 'var') && ...
-        exist('alpha_angle', 'var') && ...
-        exist('snr_value', 'var') && ...
-        exist('sim_num', 'var'))
+    exist('doa_amplitude_angle', 'var') && ...
+    exist('alpha_angle', 'var') && ...
+    exist('snr_value', 'var') && ...
+    exist('sim_num', 'var'))
     error('Missing required variables.');
 end
 
@@ -66,7 +66,7 @@ grid on;
 if sim_num > 1
     % 检查是否含有需要的变量
     if not(exist('doa_phase_angle_ave', 'var') && ...
-            exist('doa_amplitude_angle_ave', 'var'))
+        exist('doa_amplitude_angle_ave', 'var'))
         error('Missing doa_phase_angle_ave or doa_amplitude_angle_ave.');
     end
 
@@ -100,6 +100,49 @@ if sim_num > 1
     legend('show');
     grid on;
 end
+
+% ##########################角度误差图##########################
+figure;
+hold on; % 保持当前图形
+
+% 预定义颜色数组或使用MATLAB颜色图
+colors = lines(size(doa_phase_angle, 2)); % 'lines'是MATLAB内置的颜色图之一
+
+% 遍历所有SNR值
+for snr_index = (5:1:9)
+    % 计算时延比相测向误差的平均值
+    meanErrorPhase = mean(abs(doa_phase_angle(:, snr_index, :) - ...
+        repmat(reshape(alpha_angle, [length(alpha_angle), 1, 1]), ...
+        [1, 1, size(doa_phase_angle, 3)])), 3);
+    % 绘制时延比相测向误差曲线
+    plot(alpha_angle, meanErrorPhase, ...
+        'Color', colors(snr_index, :), ...
+        'LineWidth', 1, ...
+        'DisplayName', sprintf('%d dB', snr_value(snr_index)));
+    
+    % 计算比幅测向误差的平均值，只考虑[0, 90]度的角度范围
+    meanErrorAmplitude = mean(abs(doa_amplitude_angle(1:90, snr_index, :) - ...
+        repmat(reshape(alpha_angle(1:90), [90, 1, 1]), ...
+        [1, 1, size(doa_amplitude_angle, 3)])), 3);
+    % 绘制比幅测向误差曲线
+    plot(alpha_angle(1:90), meanErrorAmplitude, ...
+        '--', ...
+        'Color', colors(snr_index, :), ...
+        'LineWidth', 1, ...
+        'HandleVisibility', 'off');
+
+end
+
+hold off;
+title('Mean Absolute Error of DOA Estimation for All SNR Values');
+xlabel('Alpha Angle (degrees)');
+ylabel('Mean Absolute Error (degrees)');
+xlim([0 90]);
+ylim([0 5]);
+legend('show');
+grid on;
+
+
 
 % 计时结束
 toc;
